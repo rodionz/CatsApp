d62c89a235e566676e881c880e824514eeada52e
Rodion Zinovyev <rodionz@012.net.il>
2018-02-26 21:34:38 +0200

    pure pipe

--- src/app/colorfilter.pipe.ts
+++ src/app/colorfilter.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'colorfilter'
+  name: 'colorfilter',
+  pure:false
 })
 export class ColorfilterPipe implements PipeTransform {
 
